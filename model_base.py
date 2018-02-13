bcdfb2aad6fbe88563cc4d96c0a47a9f39eb09ea
LELE CHEN <lele.chen@rochester.edu>
2018-02-13 02:22:57 -0500

    g

--- model_base.py
+++ model_base.py
@@ -84,6 +84,8 @@ class Generator(nn.Module):
         self.generator = nn.Sequential(*model)
 
     def forward(self, input, audio):
+        print input.size()
+        print '======'
         image_feature = self.image_encoder(input).unsqueeze(2).repeat(1,1,audio.size(2)/4,1,1)
         audio_feature = self.audio_extractor(audio).unsqueeze(-1).repeat(1,1,1,1,image_feature.size(-1))
         new_input = image_feature + audio_feature
