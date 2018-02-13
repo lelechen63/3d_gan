cfcf58cc586e8e8fefd6edc783cfb4325a43754b
LELE CHEN <lele.chen@rochester.edu>
2018-02-13 01:10:15 -0500

    g

---
+++ base_trainer.py
@@ -0,0 +1,301 @@
+import os
+import glob
+import time
+import torch
+import torch.utils
+import torch.nn as nn
+import torchvision
+from torch.autograd import Variable
+from torch.utils.data import DataLoader
+from torch.nn.modules.module import _addindent
+import numpy as np
+
+from dataset import  LRWdataset1D_3d as LRWdataset
+from lstm_gan import Generator, Discriminator
+from tensorboard_logger import configure, log_value
+from embedding import Encoder
+class Trainer():
+    def __init__(self, config):
+        self.generator =  Generator(config.cuda)
+        self.discriminator =  Discriminator(config.cuda)
+        self.encoder = Encoder()
+        # if config.perceptual:
+        #     self.encoder.load_state_dict(torch.load('/mnt/disk1/dat/lchen63/lrw/model/embedding/encoder3_0.pth'))
+        #     for param in self.encoder.parameters():
+        #         param.requires_grad = False
+
+        print(self.generator)
+        # print torch_summarize(self.generator)
+        self.bce_loss_fn = nn.BCELoss()
+        self.l1_loss_fn =  nn.L1Loss()
+        self.mse_loss_fn = nn.MSELoss()
+
+        self.opt_g = torch.optim.Adam(filter(lambda p: p.requires_grad, self.generator.parameters()),
+            lr=config.lr, betas=(config.beta1, config.beta2))
+        # self.opt_d = torch.optim.Adam(self.discriminator.parameters(),
+        #     lr=config.lr, betas=(config.beta1, config.beta2))
+
+
+        # if config.dataset == 'grid':
+        #     self.dataset = VaganDataset(config.dataset_dir, train=config.is_train)
+        if config.dataset == 'lrw':
+            self.dataset = LRWdataset(config.dataset_dir, train=config.is_train)
+        # elif config.dataset == 'ldc':
+        #     self.dataset = LDCDataset(config.dataset_dir, train=config.is_train)
+
+        self.data_loader = DataLoader(self.dataset,
+                                      batch_size=config.batch_size,
+                                      num_workers=config.num_thread,
+                                      shuffle=True, drop_last=True)
+        data_iter = iter(self.data_loader)
+        data_iter.next()
+        self.ones = Variable(torch.ones(config.batch_size), requires_grad=False)
+        self.zeros = Variable(torch.zeros(config.batch_size), requires_grad=False)
+########multiple GPU####################
+        # if config.cuda:
+        #     device_ids = [int(i) for i in config.device_ids.split(',')]
+        #     self.generator     = nn.DataParallel(self.generator.cuda(), device_ids=device_ids)
+        #     self.discriminator = nn.DataParallel(self.discriminator.cuda(), device_ids=device_ids)
+        #     self.bce_loss_fn   = self.bce_loss_fn.cuda()
+        #     self.mse_loss_fn   = self.mse_loss_fn.cuda()
+        #     self.ones          = self.ones.cuda()
+#########single GPU#######################
+
+        if config.cuda:
+            device_ids = [int(i) for i in config.device_ids.split(',')]
+            self.generator     = self.generator.cuda()
+            self.discriminator = self.discriminator.cuda()
+            self.encoder = self.encoder.cuda()
+            self.bce_loss_fn   = self.bce_loss_fn.cuda()
+            self.mse_loss_fn   = self.mse_loss_fn.cuda()
+            self.ones          = self.ones.cuda()
+            self.zeros         = self.zeros.cuda()
+
+
+
+        self.config = config
+        self.start_epoch = 0
+
+        if config.load_model:
+            self.start_epoch = config.start_epoch
+            self.load(config.pretrained_dir, config.pretrained_epoch)
+
+    def fit(self):
+        config = self.config
+        configure("{}".format(config.log_dir), flush_secs=5)
+
+        num_steps_per_epoch = len(self.data_loader)
+        cc = 0
+        config.perceptual = False
+        
+
+        for epoch in range(self.start_epoch, config.max_epochs):
+            for step, (example_image, example_lms, right_imgs, right_lmss, wrong_imgs, wrong_lmss) in enumerate(self.data_loader):
+                t1 = time.time()
+
+                if config.cuda:
+                    example_image = Variable(example_image.float()).cuda()
+                    example_lms = Variable(example_lms.float()).cuda()
+                    right_lmss = Variable(right_lmss.float()).cuda()
+                    right_imgs    = Variable(right_imgs.float()).cuda()
+                    wrong_imgs = Variable(wrong_imgs.float()).cuda()
+                    wrong_lmss = Variable(wrong_lmss.float()).cuda()
+
+
+                    
+                else:
+                    example_image = Variable(example_image.float())
+                    example_lms = Variable(example_lms.float())
+                    right_lmss = Variable(right_lmss.float())
+                    right_imgs    = Variable(right_imgs.float())
+                    wrong_imgs = Variable(wrong_imgs.float())
+                    wrong_lmss = Variable(wrong_lmss.float())
+
+                    
+                
+
+                fake_im = self.generator(example_lips, landmarks, example_landmark)
+
+               
+
+                # #train the discriminator
+
+                # D_real = self.discriminator(real_im,landmarks)
+
+                # D_wrong = self.discriminator(real_im,wrong_landmarks)
+
+                # D_fake = self.discriminator(fake_im.detach(),landmarks)
+
+
+                # loss_real = self.bce_loss_fn(D_real, self.ones)
+                # loss_wrong = self.bce_loss_fn(D_wrong, self.zeros)
+                # loss_fake = self.bce_loss_fn(D_fake, self.zeros)
+
+                # loss_disc = loss_real + 0.5*(loss_wrong + loss_fake)
+                # loss_disc.backward()
+                # self.opt_d.step()
+                # self._reset_gradients()
+
+
+                #train the generator
+                # fake_im = self.generator(example_lips, landmarks)
+                # # print fake_im.size()
+                # D_fake = self.discriminator(fake_im, landmarks)
+
+                # loss_gen = self.bce_loss_fn(D_fake, self.ones)
+                loss_gen = self.l1_loss_fn(fake_im,real_im)
+                loss = loss_gen
+                loss.backward()
+                self.opt_g.step()
+                self._reset_gradients()
+
+                t2 = time.time()
+
+                if (step+1) % 10 == 0 or (step+1) == num_steps_per_epoch:
+                    steps_remain = num_steps_per_epoch-step+1 + \
+                        (config.max_epochs-epoch+1)*num_steps_per_epoch
+                    eta = int((t2-t1)*steps_remain)
+                    if config.perceptual:
+                        print("[{}/{}][{}/{}]   Loss_G: {:.4f}, loss_perceptual: {:.4f}  ETA: {} second"
+                              .format(epoch+1, config.max_epochs,
+                                      step+1, num_steps_per_epoch, loss_gen.data[0], loss_perc.data[0],  eta))
+                        log_value('generator_loss',loss_gen.data[0] , step + num_steps_per_epoch * epoch)
+                    else:
+
+                        print("[{}/{}][{}/{}]   Loss_G: {:.4f},  ETA: {} second"
+                              .format(epoch+1, config.max_epochs,
+                                      step+1, num_steps_per_epoch, loss_gen.data[0],  eta))
+                        log_value('generator_loss',loss_gen.data[0] , step + num_steps_per_epoch * epoch)
+                if (step ) % (num_steps_per_epoch/50) == 0 :
+                    # fake_store = fake_im.data.permute(0,2,1,3,4).contiguous().view(config.batch_size*29,3,64,64)
+                    torchvision.utils.save_image(fake_im.data,
+                        "{}fake_{}.png".format(config.sample_dir,cc),normalize=True)
+                    # real_store = real_im.data.permute(0,2,1,3,4).contiguous().view(config.batch_size*29,3,64,64)
+                    torchvision.utils.save_image(real_im.data,
+                        "{}real_{}.png".format(config.sample_dir,cc),normalize=True)
+                    cc += 1
+            
+                    torch.save(self.generator.state_dict(),
+                               "{}/r_generator_{}.pth"
+                               .format(config.model_dir,cc))
+
+    def load(self, directory, epoch):
+        gen_path = os.path.join(directory, 'generator_{}.pth'.format(epoch))
+
+        self.generator.load_state_dict(torch.load(gen_path))
+
+        dis_path = os.path.join(directory, 'discriminator_{}.pth'.format(epoch))
+
+        self.discriminator.load_state_dict(torch.load(dis_path))
+
+        print("Load pretrained [{}, {}]".format(gen_path, disc_path))
+
+    def _reset_gradients(self):
+        self.generator.zero_grad()
+        # self.discriminator.zero_grad()
+
+
+
+
+import argparse
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--lr",
+                        type=float,
+                        default=0.0002)
+    parser.add_argument("--beta1",
+                        type=float,
+                        default=0.5)
+    parser.add_argument("--beta2",
+                        type=float,
+                        default=0.999)
+    parser.add_argument("--lambda1",
+                        type=int,
+                        default=100)
+    parser.add_argument("--batch_size",
+                        type=int,
+                        default=64)
+    parser.add_argument("--noise_size",
+                        type=int,
+                        default=0)
+    parser.add_argument("--max_epochs",
+                        type=int,
+                        default=5)
+    parser.add_argument("--cuda",
+                        default=True)
+    parser.add_argument("--dataset_dir",
+                        type=str,
+                        default="/mnt/disk1/dat/lchen63/lrw/data/pickle/")
+                        # default = '/media/lele/DATA/lrw/data2/pickle')
+    parser.add_argument("--model_dir",
+                        type=str,
+                        default="/mnt/disk1/dat/lchen63/lrw/model/model_gan_r")
+                        # default='/media/lele/DATA/lrw/data2/model')
+    parser.add_argument("--sample_dir",
+                        type=str,
+                        default="/mnt/disk1/dat/lchen63/lrw/sample/model_gan_r/")
+                        # default='/media/lele/DATA/lrw/data2/sample/lstm_gan')
+    parser.add_argument("--log_dir",
+                        type=str,
+                        default="/mnt/disk1/dat/lchen63/data/lrw/data/log/")
+                        # default="/media/lele/DATA/lrw/data2/log/lstm_gan/")
+    parser.add_argument('--device_ids', type=str, default='2')
+    parser.add_argument('--dataset', type=str, default='lrw')
+    parser.add_argument('--num_thread', type=int, default=8)
+    # parser.add_argument('--flownet_pth', type=str, help='path of flownets model')
+    parser.add_argument('--weight_decay', type=float, default=4e-4)
+    parser.add_argument('--lr_corr', type=float, default=0.0001)
+    parser.add_argument('--lr_flownet', type=float, default=1e-4)
+    parser.add_argument('--fake_corr', type=bool, default=True)
+    parser.add_argument('--load_model', action='store_true')
+    parser.add_argument('--pretrained_dir', type=str) 
+    parser.add_argument('--pretrained_epoch', type=int)
+    parser.add_argument('--start_epoch', type=int, default=0, help='start from 0')
+    parser.add_argument('--perceptual', type=bool, default=False)
+
+    return parser.parse_args()
+
+
+def main(config):
+    t = trainer.Trainer(config)
+    t.fit()
+
+# def torch_summarize(model, show_weights=True, show_parameters=True):
+#     """Summarizes torch model by showing trainable parameters and weights."""
+#     tmpstr = model.__class__.__name__ + ' (\n'
+#     for key, module in model._modules.items():
+#         # if it contains layers let call it recursively to get params and weights
+#         if type(module) in [
+#             torch.nn.modules.container.Container,
+#             torch.nn.modules.container.Sequential
+#         ]:
+#             modstr = torch_summarize(module)
+#         else:
+#             modstr = module.__repr__()
+#         modstr = _addindent(modstr, 2)
+
+#         params = sum([np.prod(p.size()) for p in module.parameters()])
+#         weights = tuple([tuple(p.size()) for p in module.parameters()])
+
+#         tmpstr += '  (' + key + '): ' + modstr 
+#         if show_weights:
+#             tmpstr += ', weights={}'.format(weights)
+#         if show_parameters:
+#             tmpstr +=  ', parameters={}'.format(params)
+#         tmpstr += '\n'   
+
+#     tmpstr = tmpstr + ')'
+#     return tmpstr
+
+if __name__ == "__main__":
+    config = parse_args()
+    config.is_train = 'train'
+    import base_trainer as trainer
+    if not os.path.exists(config.model_dir):
+        os.mkdir(config.model_dir)
+    if not os.path.exists(config.sample_dir):
+        os.mkdir(config.sample_dir)
+    os.environ["CUDA_VISIBLE_DEVICES"] = config.device_ids
+
+    main(config)
---
+++ dataset.py
@@ -0,0 +1,214 @@
+import os
+import random
+import pickle
+import numpy as np
+import cv2
+import torch
+import torch.utils.data as data
+import torchvision.transforms as transforms
+from PIL import Image
+
+class LRWdataset1D_3d(data.Dataset):
+    def __init__(self,
+                 dataset_dir,
+                 output_shape=[128, 128],
+                 train='train'):
+        self.train = train
+        self.dataset_dir = dataset_dir
+        self.output_shape = tuple(output_shape)
+
+        if not len(output_shape) in [2, 3]:
+            raise ValueError("[*] output_shape must be [H,W] or [C,H,W]")
+
+        if self.train=='train':
+            _file = open(os.path.join(dataset_dir, "new_video_train.pkl"), "rb")
+            self.train_data = pickle.load(_file)
+            _file.close()
+        elif self.train =='test':
+            _file = open(os.path.join(dataset_dir, "new_video_test.pkl"), "rb")
+            self.test_data = pickle.load(_file)
+            _file.close()
+        elif self.train =='demo' :
+            _file = open(os.path.join(dataset_dir, "img_demo.pkl"), "rb")
+            self.demo_data = pickle.load(_file)
+            _file.close()
+
+        self.transform = transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Normalize(mean=(0.5, 0.5, 0.5), std=(0.5, 0.5, 0.5))
+        ])
+
+    def __getitem__(self, index):
+        # In training phase, it return real_image, wrong_image, text
+        if self.train=='train':
+        
+            while True:
+                # try:
+                    wrong_index = random.choice(
+                        [x for x in range(self.__len__()) if x != index])
+                    #load righ img
+                    right_imgs = torch.FloatTensor(8,128,128,3)
+                    right_lmss = torch.FloatTensor(1,32,128)
+                    # right_landmarks = torch.FloatTensor(8,68,2)
+
+                    wrong_imgs = torch.FloatTensor(8,128,128,3)
+                    wrong_lmss = torch.FloatTensor(1, 32,128)
+                    # wrong_landmark = torch.FloatTensor(8,68,2)
+
+                    for i in  range(0,8):
+                        image_path = self.train_data[index][i*3]
+                        lms_path = self.train_data[index][1 + i*3]
+                        # landmark_path = self.train_data[index][2 + i*3]
+                        im = cv2.imread(image_path)
+                        if im is None:
+                            raise IOError
+                        im = cv2.cvtColor(im, cv2.COLOR_BGR2RGB)
+                        im = cv2.resize(im, self.output_shape)
+                        im = self.transform(im)
+                        right_img = torch.FloatTensor(im)
+                        # right_landmark = torch.FloatTensor(np.load(landmark_path))
+                        right_lms = torch.FloatTensor(np.load(lms_path))
+                        right_imgs[i,:,:,:] = right_img
+                        # right_landmarks[i,:,:] =  right_landmark
+                        right_lmss[0,i*4:(i+1) * 4,:] =  right_lms
+
+
+                        image_path = self.train_data[wrong_index][i*3]
+                        lms_path = self.train_data[wrong_index][1 + i*3]
+                        # landmark_path = self.train_data[wrong_index][2 + i*3]
+                        im = cv2.imread(image_path)
+                        if im is None:
+                            raise IOError
+                        im = cv2.cvtColor(im, cv2.COLOR_BGR2RGB)
+                        im = cv2.resize(im, self.output_shape)
+                        im = self.transform(im)
+                        wrong_img = torch.FloatTensor(im)
+                        # wrong_landmark = torch.FloatTensor(np.load(landmark_path))
+                        wrong_lms = torch.FloatTensor(np.load(lms_path))
+                        wrong_imgs[i,:,:,:] = wrong_img
+                        # wrong_landmarks[i,:,:,:] =  wrong_landmark
+                        wrong_lmss[0,i*4:(i+1) * 4,:] =  wrong_lms
+
+                    example_image = right_imgs[0]
+                    example_landmark = right_landmarks[0]
+                    example_lms = right_lmss[0]
+
+                    # return  example_landmark, example_lms,right_landmarks, right_lmss
+                  
+                    # return example_image, example_landmark, example_lms, right_imgs,right_landmarks, right_lmss
+                    return example_image, example_lms, right_imgs, right_lmss, wrong_imgs,wrong_lmss
+
+                    # return example_image, example_landmark, example_lms, right_imgs,right_landmarks, right_lmss, wrong_imgs, wrong_landmarks,wrong_lmss
+                # except:
+                #     index = (index + 1) % len(self.train_data)
+
+                    # print 'Fuck'
+
+        elif self.train =='test':
+            while True:
+                # try:
+                    wrong_index = random.choice(
+                        [x for x in range(self.__len__()) if x != index])
+                    #load righ img
+                    right_imgs = torch.FloatTensor(8,128,128,3)
+                    right_lmss = torch.FloatTensor(1,32,128)
+                    # right_landmarks = torch.FloatTensor(8,68,2)
+
+                    wrong_imgs = torch.FloatTensor(8,128,128,3)
+                    wrong_lmss = torch.FloatTensor(1,32,128)
+                    # wrong_landmark = torch.FloatTensor(8,68,2)
+
+                    for i in  range(0,8):
+                        image_path = self.train_data[index][i*3]
+                        lms_path = self.test_data[index][1 + i*3]
+                        landmark_path = self.test_data[index][2 + i*3]
+                        im = cv2.imread(image_path)
+                        if im is None:
+                            raise IOError
+                        im = cv2.cvtColor(im, cv2.COLOR_BGR2RGB)
+                        im = cv2.resize(im, self.output_shape)
+                        im = self.transform(im)
+                        right_img = torch.FloatTensor(im)
+                        # right_landmark = torch.FloatTensor(np.load(landmark_path))
+                        right_lms = torch.FloatTensor(np.load(lms_path))
+                        right_imgs[i,:,:,:] = right_img
+                        # right_landmarks[i,:,:] =  right_landmark
+                        right_lmss[0,i*4:(i+1) * 4,:] =  right_lms
+
+
+                        image_path = self.train_data[wrong_index][i*3]
+                        lms_path = self.train_data[wrong_index][1 + i*3]
+                        # landmark_path = self.train_data[wrong_index][2 + i*3]
+                        im = cv2.imread(image_path)
+                        if im is None:
+                            raise IOError
+                        im = cv2.cvtColor(im, cv2.COLOR_BGR2RGB)
+                        im = cv2.resize(im, self.output_shape)
+                        im = self.transform(im)
+                        wrong_img = torch.FloatTensor(im)
+                        # wrong_landmark = torch.FloatTensor(np.load(landmark_path))
+                        wrong_lms = torch.FloatTensor(np.load(lms_path))
+                        wrong_imgs[i,:,:,:] = wrong_img
+                        # wrong_landmarks[i,:,:,:] =  wrong_landmark
+                        wrong_lmss[0,i*4:(i+1) * 4,:] =  wrong_lms
+
+                    example_image = right_imgs[0]
+                    # example_landmark = right_landmarks[0]
+                    example_lms = right_lmss[0]
+                    
+                    # return  example_landmark, example_lms,right_landmarks, right_lmss
+                    # return example_image, example_landmark, example_lms, right_imgs,right_landmarks, right_lmss
+
+                    return example_image, example_lms, right_imgs, right_lmss, wrong_imgs, wrong_lmss
+                   
+
+                    # return example_image, example_landmark, example_lms, right_imgs,right_landmarks, right_lmss, wrong_imgs, wrong_landmarks,wrong_lmss
+                # except:
+                #     index = (index + 1) % len(self.train_data)
+        elif  self.train =='demo':
+            while True:
+                # try:
+                
+                    image_path = self.demo_data[index][0]
+                    landmark_path = self.demo_data[index][2]
+                    im = cv2.imread(image_path)
+                    im = cv2.cvtColor(im, cv2.COLOR_BGR2RGB)
+                    im = cv2.resize(im, self.output_shape)
+                    im = self.transform(im)
+
+                    right_img = torch.FloatTensor(im)
+
+
+                    #load example image
+                    
+                    example_path = image_path.split('_')[0] + '_00001.jpg'
+                    example_landmark = np.load(landmark_path.split('_')[0] + '_00001.npy')
+
+                    # example_path = '/mnt/disk1/dat/lchen63/lrw/demo/obama_cartoon_region.jpg'
+                    # example_landmark = np.load('/mnt/disk1/dat/lchen63/lrw/demo/obama_cartoon.npy')
+
+                    example_lip = cv2.imread(example_path)
+                 
+                    example_lip = cv2.cvtColor(example_lip, cv2.COLOR_BGR2RGB)
+                    example_lip = cv2.resize(example_lip, self.output_shape)
+                    example_lip = self.transform(example_lip)
+
+
+                    land_path = self.demo_data[index][2]
+                    right_landmark = torch.FloatTensor(np.load(landmark_path))
+      
+                    wrong_landmark = right_landmark
+                    return example_lip, example_landmark, right_img,right_landmark, wrong_landmark
+                    # except:
+                    #     print '#############'
+                    #     ndex = (index + 1) % len(self.demo_data)
+                    # continue
+                    # print 'Fuck'
+
+    def __len__(self):
+        if self.train=='train':
+            return len(self.train_data)
+        elif self.train=='test':
+            return len(self.test_data)
+        else:
+            return len(self.demo_data)
