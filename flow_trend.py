44a522fa076bc7d65067b9f6ad577aa88b91a261
Zhiheng Li <hubertlee915@gmail.com>
2018-03-09 00:19:52 +0800

    fix indent bug

--- flow_trend.py
+++ flow_trend.py
@@ -6,21 +6,21 @@ import cPickle as pickle
 
 def worker(line):
     video_name = line  # used as key in dict
-        line = line.replace('video', 'regions')
-        line = line.replace('lipread_vgg', 'lrw')
-        frames_folder = line.split('.')[0]
-        frame_paths = sorted([f for f in os.listdir(frames_folder) if f.endswith('#lip.jpg')])
-        
-        prev = None
-        mean_flows = []
-        for frame_path in frame_paths:
-            cur = cv2.imread(frame_path)
-            if not prev is None:
-                flow = cv2.calcOpticalFlowFarneback(prev, cur, None, 0.5, 3, 15, 3, 5, 1.2, 0)
-                displacements = np.sqrt(of[:, :, 0]**2 + of[:, :, 1]**2)
-                value = np.mean(displacements)
-                mean_flows.append(value)                
-            prev = cur
+    line = line.replace('video', 'regions')
+    line = line.replace('lipread_vgg', 'lrw')
+    frames_folder = line.split('.')[0]
+    frame_paths = sorted([f for f in os.listdir(frames_folder) if f.endswith('#lip.jpg')])
+    
+    prev = None
+    mean_flows = []
+    for frame_path in frame_paths:
+        cur = cv2.imread(frame_path)
+        if not prev is None:
+            flow = cv2.calcOpticalFlowFarneback(prev, cur, None, 0.5, 3, 15, 3, 5, 1.2, 0)
+            displacements = np.sqrt(of[:, :, 0]**2 + of[:, :, 1]**2)
+            value = np.mean(displacements)
+            mean_flows.append(value)                
+        prev = cur
     print(video_name)
     return video_name, mean_flows
 
