0b5a78706a5b73c18a44109d0be5d79af4f42601
LELE CHEN <lele.chen@rochester.edu>
2018-03-10 15:45:04 -0500

    inf

--- gg.py
+++ gg.py
@@ -5,12 +5,15 @@ _file = open(a, "rb")
 train_data = pickle.load(_file)
 count = 0
 total = len(train_data)
-for index in range(len(train_data)):
+for index in range(total):
+	f =0
 	for i in  range(0,16):
 		lms_path = train_data[index][1 + i*3]
 		melFrames = np.transpose(np.load(lms_path))
 		# print melFrames[0][0]
         if np.isinf(melFrames[0][0]):
-            count += 1 
-            print "{}/{}".format(count,total)
-            break
+            f = 1
+            
+    if f == 1:
+    	count += 1 
+    print '{}/{}'.format(count,total)
