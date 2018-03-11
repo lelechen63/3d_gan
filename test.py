8a8610dea73ddfbfe863d59439ffb9d7ef5d3447
LELE CHEN <lele.chen@rochester.edu>
2018-03-10 22:08:16 -0500

    eps

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ import sys
 sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
 import glob
 import torch
-import gauss
+# import gauss
 
 import torchvision
 from torch.autograd import Variable
@@ -67,68 +67,68 @@ def block_process(A, block):
             flatten_contrast.append(block_view)
     block_contrast = np.array(flatten_contrast).reshape(block_contrast.shape)
     return block_contrast
-def ssim(img1, img2, cs_map=False):
-    """Return the Structural Similarity Map corresponding to input images img1 
-    and img2 (images are assumed to be uint8)
+# def ssim(img1, img2, cs_map=False):
+#     """Return the Structural Similarity Map corresponding to input images img1 
+#     and img2 (images are assumed to be uint8)
     
-    This function attempts to mimic precisely the functionality of ssim.m a 
-    MATLAB provided by the author's of SSIM
-    https://ece.uwaterloo.ca/~z70wang/research/ssim/ssim_index.m
-    """
-    img1 = img1.astype(numpy.float64)
-    img2 = img2.astype(numpy.float64)
-    size = 11
-    sigma = 1.5
-    window = gauss.fspecial_gauss(size, sigma)
-    K1 = 0.01
-    K2 = 0.03
-    L = 255 #bitdepth of image
-    C1 = (K1*L)**2
-    C2 = (K2*L)**2
-    mu1 = signal.fftconvolve(window, img1, mode='valid')
-    mu2 = signal.fftconvolve(window, img2, mode='valid')
-    mu1_sq = mu1*mu1
-    mu2_sq = mu2*mu2
-    mu1_mu2 = mu1*mu2
-    sigma1_sq = signal.fftconvolve(window, img1*img1, mode='valid') - mu1_sq
-    sigma2_sq = signal.fftconvolve(window, img2*img2, mode='valid') - mu2_sq
-    sigma12 = signal.fftconvolve(window, img1*img2, mode='valid') - mu1_mu2
-    if cs_map:
-        return (((2*mu1_mu2 + C1)*(2*sigma12 + C2))/((mu1_sq + mu2_sq + C1)*
-                    (sigma1_sq + sigma2_sq + C2)), 
-                (2.0*sigma12 + C2)/(sigma1_sq + sigma2_sq + C2))
-    else:
-        return ((2*mu1_mu2 + C1)*(2*sigma12 + C2))/((mu1_sq + mu2_sq + C1)*
-                    (sigma1_sq + sigma2_sq + C2))
+#     This function attempts to mimic precisely the functionality of ssim.m a 
+#     MATLAB provided by the author's of SSIM
+#     https://ece.uwaterloo.ca/~z70wang/research/ssim/ssim_index.m
+#     """
+#     img1 = img1.astype(numpy.float64)
+#     img2 = img2.astype(numpy.float64)
+#     size = 11
+#     sigma = 1.5
+#     window = gauss.fspecial_gauss(size, sigma)
+#     K1 = 0.01
+#     K2 = 0.03
+#     L = 255 #bitdepth of image
+#     C1 = (K1*L)**2
+#     C2 = (K2*L)**2
+#     mu1 = signal.fftconvolve(window, img1, mode='valid')
+#     mu2 = signal.fftconvolve(window, img2, mode='valid')
+#     mu1_sq = mu1*mu1
+#     mu2_sq = mu2*mu2
+#     mu1_mu2 = mu1*mu2
+#     sigma1_sq = signal.fftconvolve(window, img1*img1, mode='valid') - mu1_sq
+#     sigma2_sq = signal.fftconvolve(window, img2*img2, mode='valid') - mu2_sq
+#     sigma12 = signal.fftconvolve(window, img1*img2, mode='valid') - mu1_mu2
+#     if cs_map:
+#         return (((2*mu1_mu2 + C1)*(2*sigma12 + C2))/((mu1_sq + mu2_sq + C1)*
+#                     (sigma1_sq + sigma2_sq + C2)), 
+#                 (2.0*sigma12 + C2)/(sigma1_sq + sigma2_sq + C2))
+#     else:
+#         return ((2*mu1_mu2 + C1)*(2*sigma12 + C2))/((mu1_sq + mu2_sq + C1)*
+#                     (sigma1_sq + sigma2_sq + C2))
 
-def msssim_f(img1, img2):
-    """This function implements Multi-Scale Structural Similarity (MSSSIM) Image 
-    Quality Assessment according to Z. Wang's "Multi-scale structural similarity 
-    for image quality assessment" Invited Paper, IEEE Asilomar Conference on 
-    Signals, Systems and Computers, Nov. 2003 
+# def msssim_f(img1, img2):
+#     """This function implements Multi-Scale Structural Similarity (MSSSIM) Image 
+#     Quality Assessment according to Z. Wang's "Multi-scale structural similarity 
+#     for image quality assessment" Invited Paper, IEEE Asilomar Conference on 
+#     Signals, Systems and Computers, Nov. 2003 
     
-    Author's MATLAB implementation:-
-    http://www.cns.nyu.edu/~lcv/ssim/msssim.zip
-    """
-    level = 5
-    weight = np.array([0.0448, 0.2856, 0.3001, 0.2363, 0.1333])
-    downsample_filter = np.ones((2, 2))/4.0
-    im1 = img1.astype(np.float64)
-    im2 = img2.astype(np.float64)
-    mssim = np.array([])
-    mcs = np.array([])
-    for l in range(level):
-        ssim_map, cs_map = ssim(im1, im2, cs_map=True)
-        mssim = np.append(mssim, ssim_map.mean())
-        mcs = np.append(mcs, cs_map.mean())
-        filtered_im1 = ndimage.filters.convolve(im1, downsample_filter, 
-                                                mode='reflect')
-        filtered_im2 = ndimage.filters.convolve(im2, downsample_filter, 
-                                                mode='reflect')
-        im1 = filtered_im1[::2, ::2]
-        im2 = filtered_im2[::2, ::2]
-    return (np.prod(mcs[0:level-1]**weight[0:level-1])*
-                    (mssim[level-1]**weight[level-1]))
+#     Author's MATLAB implementation:-
+#     http://www.cns.nyu.edu/~lcv/ssim/msssim.zip
+#     """
+#     level = 5
+#     weight = np.array([0.0448, 0.2856, 0.3001, 0.2363, 0.1333])
+#     downsample_filter = np.ones((2, 2))/4.0
+#     im1 = img1.astype(np.float64)
+#     im2 = img2.astype(np.float64)
+#     mssim = np.array([])
+#     mcs = np.array([])
+#     for l in range(level):
+#         ssim_map, cs_map = ssim(im1, im2, cs_map=True)
+#         mssim = np.append(mssim, ssim_map.mean())
+#         mcs = np.append(mcs, cs_map.mean())
+#         filtered_im1 = ndimage.filters.convolve(im1, downsample_filter, 
+#                                                 mode='reflect')
+#         filtered_im2 = ndimage.filters.convolve(im2, downsample_filter, 
+#                                                 mode='reflect')
+#         im1 = filtered_im1[::2, ::2]
+#         im2 = filtered_im2[::2, ::2]
+#     return (np.prod(mcs[0:level-1]**weight[0:level-1])*
+#                     (mssim[level-1]**weight[level-1]))
 
 def cpbd_compute(image):
     if isinstance(image, str):
