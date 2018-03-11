f08a06ea746043038767f5e479041a8415fa6294
Zhiheng Li <hubertlee915@gmail.com>
2018-03-11 22:46:55 +0800

    finish lms of grid

--- multithread_lms_trend.py
+++ multithread_lms_trend.py
@@ -1,27 +1,30 @@
 from __future__ import print_function
 import numpy as np
-root = '/mnt/disk1/dat/lchen63/lrw/data/'
+root = '/mnt/disk1/dat/lchen63/grid/data/'
 import cPickle as pickle
 from multiprocessing import Pool
 import os
 
+regions_root = '/mnt/disk1/dat/lchen63/grid/data/lms/'
 
-def worker(line):
-    line = line[:-1].replace('lipread_vgg', 'lrw')
-    temp = line.split('/')
-    videoname = line
-    lms_folder_name = line.replace('video', 'lms')[:-4]
 
+def worker(video_name):
+    folder_path = regions_root + video_name
+    if not os.path.exists(folder_path):
+        return video_name, None
+    
     previous = None
     tt = []
-    for i in range(1, 30):
-        cur_fname = lms_folder_name + '/' + temp[-1][:-4] + '_%03d.npy' % i
+    for i in range(1, 76):
+        cur_fname = os.path.join(folder_path, video_name + '_%03d.npy' % i)
         if not os.path.exists(cur_fname):
-            return videoname, None
+            print('path not exists: {}'.format(cur_fname))
+            return video_name, None
         
         cur = np.load(cur_fname)
         if np.any(np.isinf(cur)):
-            return videoname, None
+            print('has inf: {}'.format(video_name))
+            return video_name, None
 
         if previous is not None:
             value = np.mean(cur - previous)
@@ -29,22 +32,23 @@ def worker(line):
         
         previous = cur
 
-	print(videoname)
-	return videoname, tt
+	print(video_name)
+	return video_name, tt
     			
     	
 if __name__ == '__main__':
-    with open(root + 'prefix.txt', 'r') as f:
-        lines = f.readlines()
-        # for line in lines:
-    # 	    videoname, tt = worker(line)
-        #     result[videoname, tt]
-        pool = Pool(40)
-        result = pool.map(worker, lines)
-        result = dict([(vname, tt)
-                       for (vname, tt) in result if tt is not None and len(tt) > 0])
+    video_names = os.listdir('/mnt/disk1/dat/lchen63/grid/data/regions/')
+    
+    
+    # for line in lines:
+# 	    videoname, tt = worker(line)
+    #     result[videoname, tt]
+    pool = Pool(40)
+    result = pool.map(worker, video_names)
+    result = dict([(vname, tt)
+                    for (vname, tt) in result if tt is not None and len(tt) == 75])
 
     # if count == 10:
     # 	break
-    with open('/mnt/disk0/dat/zhiheng/lip_movements/trend_lms.pkl', 'wb') as handle:
+    with open('/mnt/disk0/dat/zhiheng/lip_movements/grid_trend_lms.pkl', 'wb') as handle:
         pickle.dump(result, handle)
