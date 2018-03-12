2dc00031adf94f8911e9f118805a6d390e9c3bbf
Zhiheng Li <hubertlee915@gmail.com>
2018-03-13 01:56:54 +0800

    use numpy abs

--- corr_offset_tab.py
+++ corr_offset_tab.py
@@ -2,7 +2,7 @@ from chunk_histo import make_delay
 import cPickle as pickle
 from scipy.stats import pearsonr
 from random import shuffle
-import math
+import numpy as np
 
 vnames = ['bbwr6p', 'bgie8n', 'bbifza', 'bbbk9p', 'bbaq7s', 'bgbs5s', 'bbaq1a', 'bbwd2a', 'bgbz3p', 'bgbg7n', 'bgak2n', 'bbwj4p', 'bbbd8s', 'bgay4n', 'bbiu8n', 'bgaf4s', 'bbik4n', 'bbwv9s', 'bbaj3s', 'bbiv3n', 'bbij9a', 'bgaf1a', 'bgbz1n', 'bgam1p', 'bbax2s', 'bbic1a', 'bbwgzp', 'bgig6a', 'bgbm7s', 'bbap8a', 'bbij5s', 'bgbfzp', 'bgar6p', 'bbwp8s', 'bbac9a', 'bbbp9s', 'bgam4n', 'bbad8p', 'bbwy7a', 'bbac7s', 'bgid7n', 'bbad7p', 'bgbz9n', 'bgbe9s', 'bgbn6s', 'bbbk7s', 'bbwf4p', 'bgba7s', 'bgaa7s', 'bgar3n', 'bgaa3p', 'bbaezp', 'bbbz8s', 'bbbe4s', 'bbie6s', 'bbae6n', 'bgah2p', 'bbwm1n', 'bbik1p', 'bgan4s', 'bbbx1n', 'bbac3n', 'bbap3p', 'bgbn4a', 'bbie8a', 'bbwz8s', 'bbbd4p', 'bbiq4s', 'bbbp2p', 'bbii2s', 'bgbs5p', 'bgba4p', 'bbaz5p', 'bgaf4n', 'bbbk1p', 'bgbtza', 'bbwc8p', 'bgbs6s', 'bgak4s', 'bbayzs', 'bbwk1s', 'bbwr2s', 'bbap2s', 'bbayzp', 'bbix3p', 'bgbf9p', 'bgbe9n', 'bgbs6n', 'bgal7n', 'bgbg2s', 'bgbe6p', 'bbwj3s', 'bbby8p', 'bbaz1a', 'bgbh3a', 'bgbf1a', 'bbil2a', 'bgbg1s', 'bbbyzn', 'bbix5p']
 shuffle(vnames)
@@ -32,6 +32,6 @@ for vname in selected_vnames:
         # lms = [(l - min(lms)) / (max(lms) - min(lms)) for l in lms]
         # flow = [(f - min(flow)) / (max(flow) - min(flow)) for f in flow]
         result[vname].append((delay, avg_corr))
-    result[vname].sort(key=lambda x: math.fabs(x), reverse=True)
+    result[vname].sort(key=lambda x: np.abs(x), reverse=True)
 
 pickle.dump(result, open('corr_offset_tab.pkl', 'wb+'))
