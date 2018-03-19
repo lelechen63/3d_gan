d57605a9081905f69747ae8f15211dd46aafe4f2
Zhiheng Li <hubertlee915@gmail.com>
2018-03-19 15:24:55 +0800

    fix rename

--- rename.py
+++ rename.py
@@ -6,10 +6,9 @@ for i in range(0, 6):
     for j in range(0, 64):
         real_frm_name = 'real_{}_{}.png'.format(i, j)
         fake_frm_name = 'fake_{}_{}.png'.format(i, j)
-        frm_idx_name = '{}.png'.format(i*64 + j)
         real_path = os.path.join(root, real_frm_name)
         fake_path = os.path.join(root, fake_frm_name)
-        real_sym = os.path.join(output, real_frm_name)
-        fake_sym = os.path.join(output, fake_frm_name)
+        real_sym = os.path.join(output, 'real_{}.png'.format(i*64 + j))
+        fake_sym = os.path.join(output, 'fake_{}.png'.format(i*64 + j))
         os.symlink(real_path, real_sym)
         os.symlink(fake_path, fake_sym)
