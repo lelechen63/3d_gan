f2f7d482953977e345884133a64b16b56f8d84c5
Zhiheng Li <hubertlee915@gmail.com>
2018-03-19 14:18:03 +0800

    fix fname

--- rename.py
+++ rename.py
@@ -4,8 +4,8 @@ root = '/mnt/disk1/dat/lchen63/lrw/test_result/model_gan_r2/'
 output = '/mnt/disk1/dat/lchen63/lrw/test_result/model_gan_r2/rename'
 for i in range(0, 6):
     for j in range(0, 64):
-        real_frm_name = 'real_{}_{}.png'
-        fake_frm_name = 'fake_{}_{}.png'
+        real_frm_name = 'real_{}_{}.png'.format(i, j)
+        fake_frm_name = 'fake_{}_{}.png'.format(i, j)
         real_path = os.path.join(root, real_frm_name)
         fake_path = os.path.join(root, fake_frm_name)
         real_sym = os.path.join(output, real_frm_name)
